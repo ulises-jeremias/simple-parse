bf24eb8c4a910341454f685c94c9ee10a1af8ae3
ulises-jeremias <ulisescf.24@gmail.com>
2018-02-14 10:42:27 -0300

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # simple-parse
+
+[![Build Status](https://travis-ci.org/ulises-jeremias/simple-parse.svg?branch=master)](https://travis-ci.org/ulises-jeremias/simple-parse)
+
 Simple, stupid and portable C argument (argv) parser.
+
+## Build
+
+Just include sparse.h to your project.
+
+SParse tests are built with CMake for all platforms. You can also use the fake configure script provided to set it up on unix:
+
+```sh
+$ ./configure
+$ make
+```
+
+## Example
+
+To handle the following program arguments:
+
+```sh
+./myprog --flag1 --flag2= --flag3=hello --flag4="Hello world"
+```
+
+You could write this:
+
+```c
+[...]
+
+int main(int argc, char* argv[])
+{
+    char *f1 = sparse_flag("flag1", "FALSE", argc, argv);         // f1 = "TRUE"
+    char *f2 = sparse_flag("flag2", "defaultval", argc, argv);    // f2 = ""
+    char *f3 = sparse_flag("flag3", "bye", argc, argv);           // f3 = "hello"
+    char *f4 = sparse_flag("flag4", "Bye world", argc, argv);     // f4 = "Hello world"
+    char *f5 = sparse_flag("flag5", "default", argc, argv);       // f5 = "default"
+    char *f6 = sparse_flag("flag6", "FALSE", argc, argv);         // f6 = "FALSE"
+
+[...]
+}
+```
--- sparse.h
+++ sparse.h
@@ -45,7 +45,7 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif // __cplusplus
+#endif /* __cplusplus */
 
 static char** SPARSE_allocated_args;
 static int SPARSE_initialized = 0;
@@ -59,7 +59,8 @@ sparse_cleanup()
                 return;
         }
 
-        if (SPARSE_current_arg == 0) {
+        if (SPARSE_current_arg == 0)
+        {
                 return;
         }
 
@@ -86,8 +87,100 @@ sparse_init()
         SPARSE_initialized = 1;
 }
 
+/**
+ * @brief Get the content of a flag if it is an assignment flag (--myflag=),
+ * the string "TRUE" if the flag is defined as boolean (--myflag)
+ * or the default value if neither assignment or boolean flag is defined.
+ *
+ * If the flag is defined multiple time, as boolean or assignment, the value of
+ * the last flag occurence is returned.
+ *
+ * @param flag The flag name
+ *
+ * @param defaultValue The default value if the flag is not found
+ *
+ * @param argc The original main(argc,_) value
+ *
+ * @param argv The original main(_,argv) value
+ *
+ * @return A pointer to a char array. It is up to the user to free() this array.
+ */
+static char*
+sparse_flag(char* name, char* defaultValue, int argc, char **argv)
+{
+        sparse_init();
+
+        /* generate the flag name */
+        char flagName[FLAG_MAX_SIZE];
+        strcpy(&flagName[0], "--");
+        strcpy(&flagName[2], name);
+        strcpy(&flagName[strlen(name) + 2], "=");
+
+        /* return value */
+        char* content = NULL;
+
+        /* iterate over argv */
+        int j;
+        for (j = 0; j < argc; j++)
+        {
+                char* arg = argv[j];
+
+                if (strncmp(flagName, arg, strlen(flagName)) == 0)
+                {
+                        /* flag found with "=" */
+
+                        if (strlen(flagName) == strlen(arg))
+                        { /* no content */
+                                if (content != NULL) free(content);
+                                content = (char*) malloc(1);
+                                *content = '\0';
+
+                                continue;
+                        }
+
+                        /* there is some content */
+                        if (content != NULL)
+                                free(content);
+
+                        content = (char*) malloc(strlen(arg) - strlen(flagName) + 1);
+                        strcpy(content, &arg[strlen(flagName)]);
+
+                }
+                else if (strncmp(flagName, arg, strlen(flagName) - 1) == 0)
+                {
+                        /* found partial flag --${name}
+                           set content to true if it is the end of the str */
+                        if (strlen(arg) == strlen(flagName) - 1)
+                        {
+                                if (content != NULL)
+                                        free(content);
+                                content = (char*) malloc(strlen("TRUE") + 1);
+                                strcpy(content, "TRUE");
+                        }
+                }
+        }
+
+        if (content == NULL)
+        { /* flag not found set default or null */
+                if (defaultValue == NULL)
+                {
+                        content = NULL;
+                }
+                else
+                {
+                        content = (char*) malloc(strlen(defaultValue) + 1);
+                        strcpy(content, defaultValue);
+                }
+        }
+
+        SPARSE_allocated_args[SPARSE_current_arg] = content;
+        SPARSE_current_arg += 1;
+
+        return content;
+}
+
 #ifdef __cplusplus
 }
-#endif // __cplusplus
+#endif /* __cplusplus */
 
-#endif // SPARSE_H
+#endif /* SPARSE_H */
